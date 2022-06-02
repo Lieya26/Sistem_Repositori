bf13376842e84617c23c544c79e8ef0ea2dc0ad2
Lieya26 <93046138+Lieya26@users.noreply.github.com>
2022-06-02 09:53:24 +0800

    Add files via upload

---
+++ senarai_app.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("mainframe/header.php")?>
+
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+    <!-- navbar -->
+    <?php include("mainframe/navbar.php")?>
+    <?php include("content/senarai_aplikasi.php")?>
+    
+
+   
+    
+
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <?php include("mainframe/scripts.php")?>
+
+
